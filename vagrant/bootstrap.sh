7c71d713d4021e552c1be17685e5c5cdc747e070
shahjahanctg <engr.mohammadshahjahan@gmail.com>
2023-10-06 18:40:34 +0600

    edit

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -19,7 +19,7 @@ sudo sed -i 's|#baseurl=http://mirror.centos.org/$contentdir/$releasever/BaseOS/
 sudo sed -i 's|mirrorlist=http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=BaseOS&infra=$infra|#mirrorlist=http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=BaseOS&infra=$infra|g' /etc/yum.repos.d/CentOS-Linux-AppStream.repo
 sudo sed -i 's|mirrorlist=http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=AppStream&infra=$infra|#mirrorlist=http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=AppStream&infra=$infra|g' /etc/yum.repos.d/CentOS-Linux-BaseOS.repo
 
-echo -e "192.168.10.10 web1 web1\n192.168.10.11 web2 web2\n192.168.10.12 lb lb\n192.168.10.13\n" >> /etc/hosts
+#echo -e "192.168.10.10 web1 web1\n192.168.10.11 web2 web2\n192.168.10.12 lb lb\n192.168.10.13\n" >> /etc/hosts
 
-sudo yum repolist
-sudo yum update --allowerasing -y
+#sudo yum repolist
+#sudo yum update --allowerasing -y
